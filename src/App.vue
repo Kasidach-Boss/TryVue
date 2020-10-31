4cc38370b39416abd16ffb60cf9bc28aa7b7222d
Kasidach-Boss <kasidachst@gmail.com>
2020-11-01 00:06:51 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-4">
+    
+      <span> {{getFullName()}} </span>
+    <div>
+      This content is  in bootstrap's container
+    </div>
+    
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
+    <button type="button" class="btn btn-secondary">
+      <i class="fas fa-address-book"></i>
+    </button>
+    <button type="button" class="btn btn-success  btn-lg">Success</button>
+    <div class="container">
+      <div class="alert-success">คุณทำสำเร็จแล้ว</div>
+      <div class="alert-warning">เอ๊ะ! มีอะไรแปลกๆ</div>
+      <div class="alert-danger">นั่นอันตราย</div>
+      <div class="alert-info">รายละเอียดของคุณ</div>
+    </div>
+    
   </div>
+  
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import MyComponent from './components/MyComponent.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      MyName: "kASidach",
+      MyLastName: "sangThong"
+    };
+  },
+  methods: {
+    getName() {
+      return (
+        this.MyName.charAt(0).toUpperCase()+ this.MyName.slice(1).toLowerCase()
+      );
+    },
+    getLastName(){
+      return(
+        this.MyLastName.charAt(0).toUpperCase() + this.MyLastName.slice(1).toLowerCase()
+      );
+    },
+    getFullName(){
+      return `${this.getName()} ${this.getLastName()}`;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+
 </style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="content">
+        <h1>This is my first component</h1>
+        <p>Hello My name is{{mypropertyName}} </p>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyComponent",
+    data() {
+        return{
+            mypropertyName: " Kasidach"
+        };
+    }
+};
+</script>
+<style>
+#content {
+    text-align: center;
+    color: blue;
+}
+</style>
